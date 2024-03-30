4747ca613ec6c5266c888ea1099a3b06c4c11488
DevWayne <160737383+FelipeBastos2@users.noreply.github.com>
2024-03-30 16:55:03 -0300

    Exercicio.java

--- Exercicio.java
+++ Exercicio.java
@@ -8,23 +8,19 @@ public class Exercicio {
         double leitura =0;
 
         for(int i= 0; i<12;i++){
-        
             System.out.println("Digite a temperatura:");
             temperatura = inicialScanner.nextDouble();
 
             while (temperatura<4 || temperatura >10) {
                 System.out.println("Apenas numeros de 4 a 10.\nDigite a temperatura:");
-                temperatura = inicialScanner.nextDouble();
-               
+                temperatura = inicialScanner.nextDouble(); 
             }
             soma +=temperatura;
             leitura++;
-        }
-        
+        }        
         double media = soma/leitura;
-        System.out.println("\nA média de hoje é: "+media+"°\n");
+        System.out.println("\nA média de hoje é: "+media+"°C\n");
 
         inicialScanner.close();
-    }
-    
+    }   
 }
